51fd9d0a9be12b9c1a9570b862b1f8a473edf623
Andrii Valchuk <andriy.valchuk@gmail.com>
2021-05-19 22:28:06 +0300

    add more ip info

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 window.onload = function() {
 $.getJSON('https://ipapi.co/json/', function(data) {
   console.log(JSON.stringify(data, null, 2));
-  $('#info').append(`<p>Ip: ${JSON.stringify(data.ip, null, 2)}</p>`);
-  $('#info').append(`<p>Version: ${JSON.stringify(data.version, null, 2)}</p>`);
-  $('#info').append(`<p>City: ${JSON.stringify(data.city, null, 2)}</p>`);
-  $('#info').append(`<p>Region: ${JSON.stringify(data.region, null, 2)}</p>`);
-  $('#info').append(`<p>Country: ${JSON.stringify(data.country_name, null, 2)}</p>`);
-  $('#info').append(`<p>Country capital: ${JSON.stringify(data.country_capital, null, 2)}</p>`);
-  $('#info').append(`<p>Latitude: ${JSON.stringify(data.latitude, null, 2)}</p>`);
-  $('#info').append(`<p>Longitude: ${JSON.stringify(data.longitude, null, 2)}</p>`);
-  
+  $('#info1').append(`<p>Ip: ${JSON.stringify(data.ip, null, 2)}</p>`);
+  $('#info1').append(`<p>Version: ${JSON.stringify(data.version, null, 2)}</p>`);
+  $('#info1').append(`<p>City: ${JSON.stringify(data.city, null, 2)}</p>`);
+  $('#info1').append(`<p>Region: ${JSON.stringify(data.region, null, 2)}</p>`);
+  $('#info1').append(`<p>Country: ${JSON.stringify(data.country_name, null, 2)}</p>`);
+  $('#info1').append(`<p>Country capital: ${JSON.stringify(data.country_capital, null, 2)}</p>`);
+  $('#info1').append(`<p>Region code: ${JSON.stringify(data.region_code, null, 2)}</p>`);
+  $('#info1').append(`<p>Country code: ${JSON.stringify(data.country_code, null, 2)}</p>`);
+  $('#info1').append(`<p>Continent code: ${JSON.stringify(data.continent_code, null, 2)}</p>`);
+  $('#info1').append(`<p>Postal: ${JSON.stringify(data.postal, null, 2)}</p>`);
+  $('#info2').append(`<p>Latitude: ${JSON.stringify(data.latitude, null, 2)}</p>`);
+  $('#info2').append(`<p>Longitude: ${JSON.stringify(data.longitude, null, 2)}</p>`);
+  $('#info2').append(`<p>Timezone: ${JSON.stringify(data.timezone, null, 2)}</p>`);
+  $('#info2').append(`<p>Utc offset: ${JSON.stringify(data.utc_offset, null, 2)}</p>`);
+  $('#info2').append(`<p>Country calling code: ${JSON.stringify(data.country_calling_code, null, 2)}</p>`);
+  $('#info2').append(`<p>Currency: ${JSON.stringify(data.currency, null, 2)}</p>`);
+  $('#info2').append(`<p>Currency name: ${JSON.stringify(data.currency_name, null, 2)}</p>`);
+  $('#info2').append(`<p>Languages: ${JSON.stringify(data.languages, null, 2)}</p>`);
+  $('#info2').append(`<p>Asn: ${JSON.stringify(data.asn, null, 2)}</p>`);
+  $('#info2').append(`<p>Org: ${JSON.stringify(data.org, null, 2)}</p>`);
 });
 };
